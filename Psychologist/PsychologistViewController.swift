7427b5951526969ed5cd8f3358420e7693d88e09
glw1900 <guoliangwei1900@gmail.com>
2015-04-07 00:04:07 -0400

    add happiness

--- Psychologist/PsychologistViewController.swift
+++ Psychologist/PsychologistViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class PsychologistViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func nothing(sender: UIButton) {
+        performSegueWithIdentifier("nothing", sender: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var destination = segue.destinationViewController as? UIViewController
+        if let navCon = destination as? UINavigationController {
+            destination = navCon.visibleViewController
+        }
+        if let hvc = destination as? HappinessViewController {
+            if let identiifer = segue.identifier {
+                switch identiifer {
+                case "sad":
+                    hvc.happiness = 0
+                case "happy":
+                    hvc.happiness = 100
+                case "nothing":
+                    hvc.happiness = 25
+                default:
+                    hvc.happiness = 50
+                }
+            }
+        }
     }
-
 
 }
 
